7198f8102f6210bcf610d04a877c07a831da73cc
Admin <khadija.katanani@gmail.com>
2024-10-18 12:54:31 +0100

    code to demonstrate the use of modulos operator, ordinary single variable combination in the print command

--- Week2/odd_even.py
+++ Week2/odd_even.py
@@ -2,7 +2,7 @@
 number = int(input("Please type a number: "))
 
 if number % 2 == 0:
-    #print("The number", number, "is even!")
-    print(f"The number {number} is even!")
+    print("The number", number, "is even!")
+    # print(f"The number {number} is even!")
 else:
     print(f"The number {number} is odd!")
