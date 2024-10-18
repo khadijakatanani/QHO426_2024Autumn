f6396782c6120874fca8792eaf11beb2a7e66ca5
Admin <khadija.katanani@gmail.com>
2024-10-18 12:52:58 +0100

    code to demonstrate the use of modulos operator

---
+++ Week2/odd_even.py
@@ -0,0 +1,8 @@
+
+number = int(input("Please type a number: "))
+
+if number % 2 == 0:
+    #print("The number", number, "is even!")
+    print(f"The number {number} is even!")
+else:
+    print(f"The number {number} is odd!")
